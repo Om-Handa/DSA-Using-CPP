70bae9f71423c24f04c779b3b22c6b7d7b59d5ea
Om-handa <omhanda246@gmail.com>
2026-05-18 18:09:47 +0530

    Updated  Stock Buy and Sell

--- Arrays/stockBuySell.cpp
+++ Arrays/stockBuySell.cpp
@@ -7,18 +7,14 @@ int stockBuySell(vector<int> arr){
     int minv=INT_MAX;
 
     for(int i=0; i<arr.size(); i++){
-        if(arr[i]<minv){
-            minv=arr[i];
-        }
-        else{
-            maxv=max(maxv, arr[i]-minv);
-        }
+        minv=min(arr[i],minv);
+        maxv=max(maxv, arr[i]-minv);
     }
     return maxv;
 }
 
 int main(){
-    vector<int> arr={7,1,5,3,6,4};
+    vector<int> arr={7,6,4,3,1};
     int res= stockBuySell(arr);
     cout<<"Maximum Profit is: "<<res;
     return 0;
